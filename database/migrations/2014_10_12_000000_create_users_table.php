ce549f5e5bb06911466fbfb4c23e5ff10e1939eb
marineshale <marishane@hotmail.com>
2017-07-24 16:58:43 +0800

    version 1.0001

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('emp_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('leave_bal');
+            $table->string('role');
+            $table->string('emp_status');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        $limit = 23;
+
+        for ($i=0; $i < $limit; $i++) { 
+        	DB::table('users')->insert([
+        		'emp_name' => $faker->name,
+        		'email' => $faker->unique()->companyEmail,
+        		'password' => bcrypt('secret'),
+        		'leave_bal' => 24,
+        		'role' => 'regular',
+        		'emp_status'=>'active',
+        	]);
+        }
+    }
+}
